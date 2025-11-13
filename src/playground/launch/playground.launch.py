d3f28c13fba6561905af122d9a2296870a03ffd3
stepanove <stepanove@gmail.com>
2025-11-13 22:08:13 +0000

    Add GPS based localization

--- src/playground/launch/playground.launch.py
+++ src/playground/launch/playground.launch.py
@@ -27,7 +27,8 @@ def generate_launch_description():
 
     controller = Node(
         package=package_name,
-        executable="naive_controller",        
+        executable="naive_controller",
+        parameters=[{"use_sim_time": True}]        
     )
 
     joy = IncludeLaunchDescription(
@@ -41,7 +42,19 @@ def generate_launch_description():
                     get_package_share_directory('vrx_gz'), 'launch', 'competition.launch.py')]),
                     launch_arguments={'gz_args': ['world ', 'sydney_regatta'], 'extra_gz_args':['-v1'] }.items()
              )
+    
+    localization = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('playground'), 'launch', 'dual_ekf_navsat.launch.py')])
+             )
+
+    rviz = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('playground'), 'launch', 'rviz.launch.py')])
+             )
 
     return LaunchDescription([ros_gz_bridge, controller, joy,
                                sim,
+                               localization,
+                               rviz,
                                ])
